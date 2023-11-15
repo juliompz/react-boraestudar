dc5750f157c3ea9d12a9d981f7c3d152d35e8b49
Julio <juliokreds@gmail.com>
2023-11-15 16:45:18 -0300

    alteracoes no banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -7,28 +7,10 @@ export default function Banner () {
     return <>
         <div className="container">
             <div className="filter">
-                <p className="title">Quero estudar!</p>
+                <p className="title">O que viemos te oferecer?</p>
 
                 <div className="filter-content">
-                    <div className="filter-boxes">
-                        <div className="input-box">
-                            <p>Qual curso você quer estudar?</p>
-                            <input type="text" placeholder="ex: Análise e Desenvolvimento de Sistemas"/>
-                        </div>
-
-                        <div className="input-box">
-                            <p>Tem preferência de instituição?</p>
-                            <input type="text" placeholder="ex: Uninassau"/>
-                        </div>
-
-                        <div className="input-box">
-                            <p>Área de atuação?</p>
-                            <input type="text" placeholder="ex: Tecnologia da informação"/>
-                        </div>
-                    </div>
-
-
-                    <button type="submit">Buscar</button>
+                    
                 </div>
             </div>
         </div>
