a587333764a184d19238c6ee604e528292341cf5
Julio <juliokreds@gmail.com>
2023-11-23 14:58:28 -0300

    botao veja mais

---
+++ src/components/BotaoVejaMais/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./botaoVeja.css"
+
+export default function BotaoVejaMais(){
+    return <>
+        <div className="veja-mais">
+            <button>Veja mais</button>
+        </div>
+    </>
+}
