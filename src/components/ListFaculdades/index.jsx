1908a67f3ace104e9851d2282d57e1374b251328
Julio <juliokreds@gmail.com>
2023-11-23 14:59:21 -0300

    reformulando ListFaculdades

--- src/components/ListFaculdades/index.jsx
+++ src/components/ListFaculdades/index.jsx
@@ -2,22 +2,26 @@ import React from "react";
 import "./listFaculdades.css"
 
 import { IoStar } from "react-icons/io5";
+import BotaoVejaMais from "../BotaoVejaMais";
 
 export default function ListFaculdades({ faculdade_nome, faculdade_sigla}) {
     return <>
 
         <div className="box-faculdade">
-            <div className="faculdade-nome">
-                <h1>{faculdade_nome}</h1>
-                <div className="faculdade-estrelas">
-                    <IoStar />
-                    <IoStar />
-                    <IoStar />
-                    <IoStar />
-                    <IoStar />
+            <div className="faculdade-info">
+                <div className="faculdade-nome">
+                    <h1>{faculdade_nome}</h1>
+                    <div className="faculdade-estrelas">
+                        <IoStar />
+                        <IoStar />
+                        <IoStar />
+                        <IoStar />
+                        <IoStar />
+                    </div>
                 </div>
+                <p>{faculdade_sigla}</p>
             </div>
-            <p>{faculdade_sigla}</p>
+            <BotaoVejaMais />
         </div>  
 
     </>
