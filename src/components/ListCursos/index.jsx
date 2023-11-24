ac5341f1c25abecee1f80f47998f9cde3bf1e131
Julio <juliokreds@gmail.com>
2023-11-24 01:11:33 -0300

    ajuste minimo temporario

--- src/components/ListCursos/index.jsx
+++ src/components/ListCursos/index.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './listCursos.css'
 import { Link } from "react-router-dom";
 import BotaoVejaMais from "../BotaoVejaMais";
 
+// essa variavel 'menor' é para reaproveitar o componente utilizando outra propriedade css (diminuindo o tamanho)
+export default function ListCursos({ nomeCurso, link, qtdFaculdade }) {
 
-export default function ListCursos({ nomeCurso, link }) {
     return <>
         <Link to={ link }>
             <div className="box-curso">
                     <div className="nome-curso">
                         <h1>{nomeCurso}</h1>
-                        <p>disponível <strong>em 2 faculdades</strong></p>
+                        {/* <p>disponível <strong>em {} faculdades</strong></p> */}
                     </div>
                     <BotaoVejaMais />
                 </div>
