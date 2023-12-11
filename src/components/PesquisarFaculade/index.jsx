f02a1521faede39fa6c3f9a299cdb1bcc41447dc
Julio <juliokreds@gmail.com>
2023-12-11 13:26:25 -0300

    pesquisar faculdade

---
+++ src/components/PesquisarFaculade/index.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./pesquisarFaculdade.css"
+import blogfetch from "../../axios/config";
+
+import { useState, useEffect } from "react";
+
+export default function PesquisarFaculdades(){
+
+    const [search, setSearch] = useState("");
+    const [faculdades, setFaculdades] = useState([]);
+
+    useEffect(() => {
+        const getFaculdades = async () => {
+            try{
+                let response = await blogfetch.get('faculdades/')
+                setFaculdades(response.data)
+            }
+            catch(error){
+                console.log(error)
+            }
+        }
+        getFaculdades();
+        
+    }, []);
+
+    const searchFaculdades = faculdades.filter((curso) =>
+    curso.nome.toLowerCase().includes(search.toLowerCase())
+    );
+
+    return <>
+
+        <div className="list-container">
+            <div className="box-pesquisar">
+                <div className="title">
+                    <h1>Faculdades</h1>
+                </div>
+
+                <div className="buscar-curso">
+                    <form className="search-container">
+                        <input type="text" id="search-bar" value={search} placeholder="Pesquise uma faculdade"  onChange={(e) => setSearch(e.target.value)}/>
+                    </form>
+
+                    <div className={`box-pesquisa ${search ? 'visible' : 'hidden'}`}>
+                        <ul>
+                            {searchFaculdades.map(faculdade => (
+                                <li key={faculdade.id}>{faculdade.nome}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </>
+
+}
